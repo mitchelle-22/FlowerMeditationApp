ed382759b7c3ebc680f78b22a158a21abb981447
damac53 <mitchellefetty@gmail.com>
2023-07-04 13:22:23 +0200

    Meditation App

--- app/src/main/java/com/technical/flowermeditation/MainActivity.kt
+++ app/src/main/java/com/technical/flowermeditation/MainActivity.kt
@@ -3,13 +3,30 @@ package com.technical.flowermeditation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.technical.flowermeditation.ui.theme.FlowerMeditationTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +40,13 @@ class MainActivity : ComponentActivity() {
 //                    color = MaterialTheme.colorScheme.background
 //                ) {
 //                    Greeting("Android")
+                Column(
+
+                ){
+                    var modifier = Modifier
+                        .background(Gray)
+                        .fillMaxSize()
+                }
 
                 }
             }
@@ -31,11 +55,29 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HeaderaProfileComponent(){
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 15.dp, end = 15.dp, top = 15.dp)
+    ){
+       Row(verticalAlignment = Alignment.CenterVertically) {
+           Image(painter = painterResource(id = R.drawable.profilepicture),
+               contentDescription = "Profile Picture",
+           contentScale = ContentScale.Crop,
+           modifier = Modifier
+               .size(50.dp)
+               .clip(CircleShape)
+           )
+           Column(modifier = Modifier) {
+               
+           }
+
+           
+       }
+    }
+
 }
 
 @Preview(showBackground = true)
