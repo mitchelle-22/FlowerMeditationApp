ee1f272d3b2c1f16aaf0017d693922ff252a8c66
damac53 <mitchellefetty@gmail.com>
2023-07-24 15:25:02 +0200

    Meditation App

--- app/src/main/java/com/technical/flowermeditation/MainActivity.kt
+++ app/src/main/java/com/technical/flowermeditation/MainActivity.kt
@@ -14,12 +14,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -99,6 +101,18 @@ class MainActivity : ComponentActivity() {
              )
             }
         }
+    }
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun SearchInputComponent()
+    {
+        OutlinedTextField(
+            value = "",
+            onValueChange ={},
+            shape = RoundedCornerShape(8.dp),
+
+
+        )
     }
 }
 
