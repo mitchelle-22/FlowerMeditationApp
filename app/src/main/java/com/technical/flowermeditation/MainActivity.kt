93412b43211aa7034a33045d15d56f35c8b30d75
damac53 <mitchellefetty@gmail.com>
2023-08-04 10:46:22 +0200

    Medidation App

--- app/src/main/java/com/technical/flowermeditation/MainActivity.kt
+++ app/src/main/java/com/technical/flowermeditation/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.material3.Icon
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -20,34 +24,127 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.graphics.Color.Companion.Green
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.technical.flowermeditation.ui.theme.FlowerMeditationTheme
+
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchInputComponent() {
+    val customFontFamily = FontFamily(
+        Font(R.font.nunitolight) // Make sure 'nunitolight.ttf' is in the 'res/font' folder
+    )
+    OutlinedTextField(
+        value = "",
+        onValueChange = {},
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        placeholder = {
+            Text(
+                text = "Search",
+                fontFamily = customFontFamily,// Add the desired font family here
+            )
+        },
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")
+        },
+        trailingIcon = {
+            // Replace Icons.Filled.Search with the desired icon
+            Icon(imageVector = Icons.Default.Notifications, contentDescription = "Notifications Icon")
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 15.dp, start = 15.dp, end = 15.dp)
+            .background(Color.White, RoundedCornerShape(8.dp)),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.LightGray,
+            unfocusedBorderColor = Color.White,
+            cursorColor = Color.LightGray,
+        ),
+        textStyle = TextStyle.Default.copy(
+            fontFamily = FontFamily.Default, // Replace with the default font family or any other desired font
+            fontSize = 16.sp,
+            color = Color.Black
+        ),
+        visualTransformation = VisualTransformation.None,
+    )
+}
+@Composable
+fun Icon(icon: ImageVector, color: Color) {
+    Icon(
+        imageVector = icon,
+        contentDescription = null,
+        tint = color,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HeaderProfileComponent() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 15.dp, end = 15.dp, top = 15.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.michelle),
+                contentDescription = "Profile Picture",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+            )
+            Column(modifier = Modifier.padding(start = 10.dp)) {
+                Text(
+                    text = "Welcome back",
+                    fontFamily = FontFamily.SansSerif,
+                    fontSize = 14.sp,
+                    textAlign = TextAlign.Start
+
+                )
+                Text(
+                    text = "Michelle Ramela",
+                    fontFamily = FontFamily.Serif,
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Start
+
+                )
+            }
+
+
+        }
+        BadgedBox(badge = { Badge(Modifier.background(Green)) }) {
+            Icon(
+                Icons.Default.Notifications,
+                contentDescription = "Notification"
+            )
+        }
+    }
+}
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,157 +155,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier
                         .background(Gray)
                         .fillMaxSize()
-                ){
+                ) {
                     HeaderProfileComponent()
+                    SearchInputComponent()
 
 
                 }
 
-                }
-            }
-        }
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun HeaderProfileComponent() {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 15.dp, end = 15.dp, top = 15.dp)
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically) {
-                Image(
-                    painter = painterResource(id = R.drawable.michelle),
-                    contentDescription = "Profile Picture",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(50.dp)
-                        .clip(CircleShape)
-                )
-                Column(modifier = Modifier.padding(start = 10.dp)) {
-                    Text(
-                        text = "Welcome back",
-                        fontFamily = FontFamily.SansSerif,
-                        fontSize = 14.sp,
-                        textAlign = TextAlign.Start
-
-                    )
-                    Text(
-                        text = "Michelle Ramela",
-                        fontFamily = FontFamily.Serif,
-                        fontSize = 20.sp,
-                        textAlign = TextAlign.Start
-
-                    )
-                }
-
-
-            }
-            BadgedBox(badge = { Badge(Modifier.background(Green)) }) {
-             Icon(
-                Icons.Default.Notifications,
-                contentDescription = "Notification"
-             )
             }
         }
     }
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun SearchInputComponent()
-    {
 
-        val customFontFamily = FontFamily(
-            Font(R.font.nunitolight)
-        )
-        OutlinedTextField(value = "", onValueChange ={},
-            shape = RoundedCornerShape(8.dp),
-            placeholder = {Text(text= "Search", fontFamily = customFontFamily)},
-            leadingIcon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.filter),
-                    modifier = Modifier.size(24.dp),
-                    contentDescription = "Filter Icon",
-                    tint = Color.Gray
-                )
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 15.dp, start = 15.dp, end = 15.dp)
-                .background(Color.White),
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                focusedBorderColor = Color.LightGray,
-                unfocusedBorderColor = Color.White,
-                cursorColor = Color.LightGray,
 
-            ) ,
-
-        )
-//        val textState = remember { mutableStateOf(TextFieldValue()) }
-//
-//        val customFontFamily = FontFamily(
-//            Font(R.font.nunitolight) // Make sure 'nunitolight.ttf' is in the 'res/font' folder
-//        )
-//        OutlinedTextField(
-//            value = textState.value,
-//            onValueChange = { newValue -> textState.value = newValue },
-//            modifier = Modifier.fillMaxWidth(),
-//            shape = RoundedCornerShape(8.dp),
-//            placeholder ={
-//                Text(
-//                    text = "Search",
-//                    fontFamily = customFontFamily,// Add the desired font family here
-//                )},
-//            leadingIcon = {
-//                Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")
-//            },
-//
-//            trailingIcon = {
-//                // Replace Icons.Filled.Search with the desired icon
-//              //  CustomTrailingIcon(Icons.Filled.Search, Color.Gray)
-//
-//
-//                           },
-//
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .padding(top = 15.dp, start = 15.dp, end = 15.dp)
-//                .background(Color.White, RoundedCornerShape(8.dp)),
-//            colors = TextFieldDefaults.outlinedTextFieldColors(
-//                focusedBorderColor = Color.LightGray,
-//                unfocusedBorderColor = Color.White,
-//                cursorColor = Color.LightGray,
-//
-//            ),
-//            textStyle = TextStyle.Default.copy(
-//                fontFamily = FontFamily.Default, // Replace with the default font family or any other desired font
-//                fontSize = 16.sp,
-//                color = Color.Black
-//            ),
-//            
-//            visualTransformation = VisualTransformation.None,
-//
-//
-//
-//
-//        )
-
-    }
-    @Composable
-    fun Icon(icon: ImageVector, color: Color) {
-        Icon(
-            imageVector = icon,
-            contentDescription = null,
-            tint = color,
-        )
-
-    }
-    @Preview
-    @Composable
-    fun PreviewSearchInputComponent() {
-        SearchInputComponent()
-    }
 }
+
+
 
 
 
